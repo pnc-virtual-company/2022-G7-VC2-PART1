7ddee2f35cb6e695286255037df6414157e55972
Sophiem.LOEM <Sophiem.LOEM@pnc.lan>
2022-08-16 20:58:51 +0700

    #6 crud on student

---
+++ back/app/Http/Controllers/ClassesController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Classes;
+use Illuminate\Http\Request;
+
+class ClassesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Classes::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $class = new Classes();
+        $class->Class= $request->Class;
+
+        $class->save();
+        return response()->json(['message'=>'Successfully for create class']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Classes  $classes
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Classes  $classes
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,$id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Classes  $classes
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ back/app/Http/Controllers/RequestController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Requests;
+use Illuminate\Http\Request;
+
+class RequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // return Requests::all();
+        return Requests::with('student')->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $offer = new Requests();
+        $offer->Start_date = $request->Start_date;
+        $offer->End_date = $request->End_date;
+        $offer->Leave_Type = $request->Leave_Type;
+        $offer->Status = $request->Status;
+        $offer->student_id = $request->student_id;
+
+        $offer->save();
+        return response()->Json(['messsage'=>'Scuccfully for create Request']);
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Requests $request)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Requests $request,$id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
