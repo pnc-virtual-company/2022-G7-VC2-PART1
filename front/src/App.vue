19a1d884d80d32cd8b624fe244c5ee3416a21dae
LIDA THY <lida.thy@pnc.lan>
2022-08-16 14:50:41 +0700

    #5 menu

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,30 +1,55 @@
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<nav class="flex justify-between align-center">
+    <div class="logo my-auto">
+      <h2 class="flex justify-items-stretch ml-3" items-center>SLMS</h2>
+    </div>
+        <div class="flex justify-start align-center">
+          <router-link class="m-3 text-lg text-white text-bold" to="/">List All Leave</router-link>
+          <router-link class="m-3 text-lg text-white text-bold" to="/request">New Request</router-link>
+        </div>
+
+      <div class="flex justify-between align-center p-2">
+        <h2 class="m-2">Rady Y</h2>
+        <h2 class="m-2">Sign out</h2>
+    </div>
+</nav>
+<main><router-view/></main>
 </template>
+<script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+export default {
+
+}
+</script>
+
+<style scoped>
+body{
+  margin: 0;
+  padding: 0;
+}
+nav{
+  width: 100%;
+    background: #45b6fe;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 3px 3px 0px;
+}
+.content-nav{
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
 }
 
-nav {
-  padding: 30px;
+
+nav a.router-link-exact-active{
+  background:
+#FFAD5C;
+  border-radius: 5px;
+  padding: 5px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+nav a{
+  
+  border:none; 
+  text-decoration: none;
 }
 </style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,19 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import ListView from '../views/ListView.vue'
+import RquestView from '../views/RequestView.vue';
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'list',
+    component: ListView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/request',
+    name: 'request',
+   
+    component:RquestView
   }
 ]
 
