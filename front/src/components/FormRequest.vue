c3de0046c541492b529781b89f3d772717f1d84f
KimKy Mao <kimky.guinevere@gmail.com>
2022-08-18 11:22:54 +0700

    #0 nothing

--- front/src/components/FormRequest.vue
+++ front/src/components/FormRequest.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="container mt-5 d-flex justify-content-center">
-    <form class="w-50 p-4">
+    <form class="w-50 p-4" @submit.prevent="newRequest">
         <h1 class="text-center p-1">Request to Trainer</h1><hr>
         <label class="mt-1">StartData</label>
         <div class=" w-100 d-flex">
             <div class="form-group w-100">
-                <input type="date" placeholder="Input data" class="form-control">
+                <input type="date" placeholder="Input data" class="form-control" v-model="start_date">
             </div>
             <div class="form-group w-100 ml-2">
                 <select class="form-control">
@@ -17,9 +17,9 @@
             </div>
         </div>
         <label>EndData</label>
-        <div class=" w-100 d-flex">
+        <div class=" w-100 d-flex" >
             <div class="form-group w-100">
-                <input type="date" placeholder="Input data" class="form-control">
+                <input type="date" placeholder="Input data" class="form-control" v-model="End_date">
             </div>
             <div class="form-group w-100 ml-2">
                 <select class="form-control">
@@ -37,7 +37,7 @@
             </div>
             <div class="form-group w-100 ml-2">
                 <label>Leave type</label>
-                <select class="form-control">
+                <select class="form-control" v-model="leave_Type">
                     <option disabled value="">Choose</option>
                     <option value="">Sick</option>
                     <option>Go home</option>
@@ -48,10 +48,10 @@
         <div class=" w-100 d-flex">
             <div class="form-group w-100">
                 <label>Cause</label>
-                <input type="text" placeholder="Input data" class="form-control">
+                <input type="text" placeholder="Input data" class="form-control" v-model="Cause">
             </div>
             <div class="form-group d-flex card-btn ml-2">
-                <button class="btn btn-info">Submit</button>
+                <button class="btn btn-info" type="submit">Submit</button>
                 <button class="btn btn-danger ml-2">Cancel</button>
             </div>
         </div>
@@ -60,8 +60,37 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+    data(){
+        return{
+          start_date:"",
+          End_date:"",
+          Cause:"",
+          leave_Type:"",
+          student_id:1,
+          Padding:"Approv",
+          URL : "http://127.0.0.1:800/api/request/"
+        }
+    },
+    methods:{
+        newRequest(){
+            let date = {Start_date:this.start_date,End_date:this.End_date,Reason:this.Cause,leave_Type:this.leave_Type,
+            student_id:this.student_id,Status:this.Padding}
+            if (this.start_date !="" && this.End_date !="" && this.Cause !="" && this.Leave_Type !=""){
+                axios.post(this.URL,date).then(response => {
+                    return (response.data)
+                })
+                this.start_date = "",
+                this.End_date = "";
+                this.Cause = "";
+                this.leave_Type= "";
+                this.student_id= "";
+                this.Padding= "";
+            }
+            console.log(date)
+        } 
+    }
 }
 </script>
 
